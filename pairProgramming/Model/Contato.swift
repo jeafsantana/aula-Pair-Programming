dc02c299e01a083d65e2bf2ff2c6c9b45bc3762b
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-09-29 20:20:16 -0300

    Programação de Magno e Bruno

---
+++ pairProgramming/Model/Contato.swift
@@ -0,0 +1,9 @@
+//
+//  Contato.swift
+//  pairProgramming
+//
+//  Created by Magno Miranda Dantas on 29/09/21.
+//
+
+import Foundation
+
--- pairProgramming/ViewController.swift
+++ pairProgramming/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nomeTf: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    @IBAction func pesquisar(_ sender: Any) {
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate {}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
 
